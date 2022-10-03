4ad9e97a6f707ffe67aea5353e279a42afaaeda3
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-03 18:10:26 +0200

    feat: added a dummy setup function

--- lua/gitlens/init.lua
+++ lua/gitlens/init.lua
@@ -1,1 +1,9 @@
-print("Hello, World!")
+local M = { }
+local conf = { }
+
+function M.setup(p_conf)
+  conf = vim.tbl_deep_extend("force", conf, p_conf or {})
+  print(vim.inspect(conf))
+end
+
+return M
