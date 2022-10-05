847d6767786cdbd10dbfa71ac5a0698b750442e3
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-05 23:24:08 +0200

    perf: fixed the HACK `every thing before "local txt"...`

--- lua/gitlens/api/git.lua
+++ lua/gitlens/api/git.lua
@@ -11,23 +11,22 @@ function M.get_username()
 end
 
 function M.get_blame(file, line, int_username)
-  -- HACK: every thing before "local txt" should be in the "if hash" if statement
   local int_blame = vim.fn.system(string.format("git blame -c -L %d,%d %s", line[1], line[1], file))
   local hash = vim.split(int_blame, "%s")[1]
-
-  local username = git_cmd(string.format("git show %s ", hash).."--format='%an'")
-  if username == int_username then
-    username = "You"
-  end
-
-  local cmd = string.format("git show %s --format='%s %s'",
-    hash, username, "| %ar | %s"
-  )
 
   local txt = ""
   if hash == '00000000' then
     txt = 'Not Committed Yet'
   else
+    local username = git_cmd(string.format("git show %s ", hash).."--format='%an'")
+    if username == int_username then
+      username = "You"
+    end
+
+    local cmd = string.format("git show %s --format='%s %s'",
+      hash, username, "| %ar | %s"
+    )
+
     txt = git_cmd(cmd)
     if txt:find("fatal") then
       txt = "Not Committed Yet"
