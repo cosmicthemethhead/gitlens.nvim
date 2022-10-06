d04458f64bd1572c6ac32cad360bd1c23594b3cf
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-06 05:09:51 +0200

    feat: added some some enable/disable functions

--- lua/gitlens/init.lua
+++ lua/gitlens/init.lua
@@ -2,17 +2,15 @@ local settings = require "gitlens.settings"
 local virt_txt = require "gitlens.ui.virtual_text"
 local git = require "gitlens.api.git"
 
+local api = vim.api
 local M = { }
 
---- @param config GitlensSettings?
-function M.setup(config)
-  if config then
-    settings.set(config)
-  end
+local username = git.get_username()
 
-  local username = git.get_username()
-
-  vim.api.nvim_create_autocmd("CursorMoved", {
+function M.enable()
+  api.nvim_create_augroup("GitlensBlame", { })
+  api.nvim_create_autocmd("CursorMoved", {
+    group = "GitlensBlame",
     callback = function()
       -- HACK: checks on every cursor movement
       for _, v in ipairs(settings.current.disabled_filetypes) do
@@ -21,11 +19,25 @@ function M.setup(config)
 
       virt_txt.create_virtual_text(git.get_blame(
         vim.fn.expand("%"),
-        vim.api.nvim_win_get_cursor(0),
+        api.nvim_win_get_cursor(0),
         username
       ))
-    end
+    end,
   })
+end
+
+function M.disable()
+  api.nvim_buf_clear_namespace(0, 9, 0, -1)
+  api.nvim_del_augroup_by_name("GitlensBlame")
+end
+
+--- @param config GitlensSettings?
+function M.setup(config)
+  if config then
+    settings.set(config)
+  end
+
+  M.enable()
 end
 
 return M
